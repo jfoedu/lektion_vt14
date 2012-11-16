0bd2758ef39c64ccbc7293cc87a650b2865cf617
Joakim Forsgren <jf@freetux.park.se>
2012-11-16 14:15:13 +0100

    Lagt dit float genomrången

---
+++ float/u1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css" />
+<title>Float</title>
+</head>
+<body>
+  <div id="main">
+    <div id="nr1">Lite text 1 Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Morbi in metus ligula, vel interdum erat. Phasellus
+      vitae placerat lacus. Sed dapibus laoreet luctus. Sed iaculis lacinia
+      mi, a laoreet leo vestibulum a. Nam ante justo, dignissim placerat
+      cursus ut, malesuada quis mi. Suspendisse fringilla fringilla
+      venenatis. Nullam elementum, metus adipiscing congue luctus, ante leo
+      hendrerit turpis, eu eleifend odio erat et mi. Nunc tempor, ipsum id
+      vestibulum scelerisque, urna erat tincidunt libero, id aliquet nulla
+      ante id lacus. Aliquam suscipit, mi nec auctor pharetra, augue arcu
+      volutpat mauris, non dignissim urna lectus at enim. Vestibulum sed
+      venenatis diam. Vivamus a eleifend enim. Suspendisse accumsan mi nec
+      leo scelerisque in sodales risus dapibus. Nulla iaculis ultrices mi,
+      et commodo velit vestibulum eget. Maecenas imperdiet blandit orci sed
+      posuere. Quisque rutrum blandit suscipit. Suspendisse potenti.
+      Pellentesque habitant morbi tristique senectus et netus et malesuada
+      fames ac turpis egestas. Maecenas nec mauris mauris. Donec faucibus
+      malesuada lacinia. Cras venenatis tempus metus, id tempus massa
+      pharetra quis. Nam quis nisl diam, vitae ultricies ipsum. Ut nibh
+      lorem, eleifend vel aliquam ut, suscipit sit amet tellus. Nam egestas
+      luctus nisl vel placerat. Donec gravida tellus in tellus iaculis sed
+      commodo est tincidunt. Nulla ut ligula sit amet tellus venenatis
+      molestie. Proin consequat tempor tortor, non eleifend justo dapibus
+      vel. Cras non felis eget augue convallis interdum non in enim.
+      Curabitur malesuada mi a risus dictum sed interdum nunc interdum.
+      Vivamus faucibus, lectus non semper semper, lorem neque sollicitudin
+      augue, at ultrices ante leo a tellus. Vestibulum ultricies ultricies
+      ligula vitae volutpat.</div>
+    <div id="nr2">Lite text 2</div>
+    <div id="nr3">Lite text 3</div>
+    <div id="nr4">Lite text 4</div>
+    <br class="clear" /> Lite text vill jag ha här, det går det hela lite
+    bättre!
+  </div>
+</body>
+</html>
---
+++ float/u2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css" />
+<title>Float åäö</title>
+</head>
+<body>
+  <p>Lorem ipsum åäö dolor sit amet, consectetur adipiscing elit. Etiam
+    posuere fringilla lorem nec dictum. Donec lacinia, dui vitae aliquam
+    euismod, justo ligula tincidunt tortor, nec semper metus nulla at
+    arcu. Ut vestibulum urna vitae diam vestibulum at aliquam justo
+    aliquam. Proin metus turpis, dignissim in euismod non, mattis eget
+    justo. Suspendisse potenti. Suspendisse aliquet justo ut diam placerat
+    non pellentesque turpis ornare. Integer sit amet orci non justo
+    volutpat aliquam. Sed ut augue non risus tristique auctor. Morbi
+    rhoncus interdum semper. Maecenas tempus blandit tortor, non tincidunt
+    nunc dignissim in.</p>
+  <p id="important">Sed mi purus, tempor consequat mattis sit amet,
+    ornare euismod nibh. Etiam lorem odio, viverra vitae viverra quis,
+    scelerisque quis risus. Suspendisse vulputate urna erat, id lobortis
+    sapien. Class aptent taciti sociosqu ad litora torquent per conubia
+    nostra, per inceptos himenaeos. Quisque a nibh et nisi eleifend
+    mattis. Aliquam urna erat, adipiscing id laoreet non, fermentum ut
+    nibh. Suspendisse euismod tellus sed velit commodo in elementum odio
+    tristique. Donec sed ullamcorper mi.</p>
+  <p>Fusce elementum sapien sit amet augue tincidunt fringilla. Nam
+    laoreet lorem nec ipsum convallis aliquam. Cras vel interdum nibh.
+    Suspendisse vitae mi vel tellus hendrerit rhoncus. Cras sit amet eros
+    sit amet risus suscipit venenatis. Cras tincidunt fringilla egestas.
+    Quisque quis metus quis magna malesuada facilisis. In lacinia
+    fringilla gravida. Mauris ac nunc eros, eget feugiat turpis.</p>
+  <p id="moreimportant">Vivamus consequat purus porttitor arcu molestie
+    tempor. Aliquam erat volutpat. Donec tincidunt est massa, ut ultricies
+    risus. Maecenas accumsan leo sed elit porttitor facilisis. Praesent
+    hendrerit convallis dolor, at blandit arcu porta eget. Pellentesque
+    ligula purus, consequat a lobortis id, varius non erat. Praesent quis
+    lobortis dolor. Mauris et purus vel libero aliquam dapibus nec nec
+    enim. Sed id ipsum augue, non pharetra ipsum. Suspendisse potenti.</p>
+  <p>Nullam tristique pharetra justo, in ultricies enim pellentesque
+    quis. Integer tincidunt tempor sem, eget fringilla nisi congue non.
+    Sed pharetra congue sapien, vitae volutpat mauris dictum eget.
+    Suspendisse convallis risus sed augue varius eleifend. Proin ac
+    faucibus augue. Nam sit amet felis nisi. In a libero leo. Proin non
+    turpis neque. Quisque quis eros libero.</p>
+  <p>In tempus sem at massa tincidunt tincidunt. Integer libero turpis,
+    rhoncus vitae hendrerit at, vehicula id orci. Nam dapibus feugiat
+    lectus in consectetur. Curabitur libero arcu, interdum vel viverra
+    adipiscing, lacinia et libero. Duis quis massa ut nibh ultricies
+    ultrices. Pellentesque turpis urna, convallis sit amet luctus id,
+    dapibus in turpis. Quisque elit ante, aliquam porttitor auctor et,
+    imperdiet non ligula.</p>
+  <p id="conclusion">Nulla augue risus, mollis non auctor in, faucibus et
+    est. Integer nunc turpis, egestas ultricies aliquet eu, ultrices non
+    tellus. Cum sociis natoque penatibus et magnis dis parturient montes,
+    nascetur ridiculus mus. Quisque at turpis in arcu rutrum pellentesque
+    sed eget nunc. Suspendisse elementum, odio ut blandit ullamcorper,
+    risus eros consectetur erat, molestie scelerisque diam mauris a enim.
+    Aliquam tincidunt mauris nec massa consequat venenatis. Integer
+    viverra diam vitae nisi mollis hendrerit. Nullam lectus nunc,
+    imperdiet in mollis sit amet, mattis eu velit. Proin ipsum elit,
+    dapibus vitae commodo quis, posuere et velit. Duis venenatis, magna id
+    venenatis tincidunt, mi nisl dictum nibh, id faucibus tellus sapien
+    quis mauris. Etiam tempor euismod odio, id faucibus mi vestibulum id.
+    Donec ultricies lacinia eleifend. Nunc eget quam lacus. In mattis
+    lobortis sodales.</p>
+</body>
+</html>
